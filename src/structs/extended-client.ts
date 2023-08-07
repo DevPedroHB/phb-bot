4d15a7cb1724e05571444d222e89a2b391e47aea
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-08-06 21:00:20 -0300

    🧱 feat: Estruturas no extended client

--- src/structs/extended-client.ts
+++ src/structs/extended-client.ts
@@ -2,12 +2,24 @@ import { env } from "@/env";
 import {
   BitFieldResolvable,
   Client,
+  Collection,
   GatewayIntentsString,
   IntentsBitField,
   Partials,
 } from "discord.js";
+import {
+  CommandType,
+  ComponentsButton,
+  ComponentsModal,
+  ComponentsSelect,
+} from "./types/command";
 
 export class ExtendedClient extends Client {
+  public commands: Collection<string, CommandType> = new Collection();
+  public buttons: ComponentsButton = new Collection();
+  public selects: ComponentsSelect = new Collection();
+  public modals: ComponentsModal = new Collection();
+
   constructor() {
     super({
       intents: Object.keys(IntentsBitField.Flags) as BitFieldResolvable<
