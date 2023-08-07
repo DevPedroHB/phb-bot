9a84813fb1ce9bd04bbe0393422543cf50785864
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-08-06 22:10:32 -0300

    🎉 feat: Evento 'ready' para listar estruturas

--- src/commands/common/ping.ts
+++ src/commands/common/ping.ts
@@ -1,14 +1,39 @@
 import { Command } from "@/structs/types/command";
-import { ApplicationCommandType } from "discord.js";
+import {
+  ActionRowBuilder,
+  ApplicationCommandType,
+  ButtonBuilder,
+  ButtonStyle,
+  Collection,
+} from "discord.js";
 
 export default new Command({
   name: "ping",
   description: "reply with pong",
   type: ApplicationCommandType.ChatInput,
   run({ interaction }) {
+    const row = new ActionRowBuilder<ButtonBuilder>({
+      components: [
+        new ButtonBuilder({
+          customId: "test-button",
+          label: "Clique aqui",
+          style: ButtonStyle.Success,
+        }),
+      ],
+    });
+
     interaction.reply({
       ephemeral: true,
       content: "pong",
+      components: [row],
     });
   },
+  buttons: new Collection([
+    [
+      "test-button",
+      async (interaction) => {
+        interaction.update({ components: [] });
+      },
+    ],
+  ]),
 });
