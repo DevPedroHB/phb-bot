6993df419b72c9992a602ae676c5454b7fe8d763
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-08-06 22:00:20 -0300

    🚀 feat: Resposta em componentes e modals

---
+++ src/events/main/components.ts
@@ -0,0 +1,16 @@
+import { client } from "@/index";
+import { Event } from "@/structs/types/event";
+
+export default new Event({
+  name: "interactionCreate",
+  run(interaction) {
+    if (interaction.isModalSubmit())
+      client.modals.get(interaction.customId)?.(interaction);
+
+    if (interaction.isButton())
+      client.buttons.get(interaction.customId)?.(interaction);
+
+    if (interaction.isStringSelectMenu())
+      client.selects.get(interaction.customId)?.(interaction);
+  },
+});
